6a5d0a97be03dc8d432123d27df93a425628b72b
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-20 11:51:34 -0300

    Adicionar módulo de locals ao módulo app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FilesModule } from './files/files.module';
 import { CountriesModule } from './countries/countries.module';
 import { envConfig } from './config/env.load';
+import { LocalsModule } from './locals/locals.module';
 import * as ormConfig from './config/orm.config';
 
 @Module({
@@ -11,6 +12,7 @@ import * as ormConfig from './config/orm.config';
     TypeOrmModule.forRoot(ormConfig),
     FilesModule,
     CountriesModule,
+    LocalsModule,
   ],
 })
 export class AppModule {}
