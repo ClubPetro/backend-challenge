f2b52a4769a23d21cb693443a4893f31c65e5fd5
Pedro <pedro_nork@hotmail.com>
2021-01-21 15:19:08 -0300

    rename swagger project

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   );
 
   const swaggerOptions = new DocumentBuilder()
-    .setTitle('Engage API')
+    .setTitle('ClubPetro API')
     .setVersion('1.0')
     .build();
 
