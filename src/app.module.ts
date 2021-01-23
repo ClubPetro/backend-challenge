216c2cc07a59e3318d817e9cc2667abd15291d8a
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-23 19:35:03 -0300

    Adicionar modulo de metas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FilesModule } from './files/files.module';
 import { CountriesModule } from './countries/countries.module';
 import { envConfig } from './config/env.load';
+import { MetasModule } from './metas/metas.module';
 import * as ormConfig from './config/orm.config';
 
 @Module({
@@ -11,6 +12,7 @@ import * as ormConfig from './config/orm.config';
     TypeOrmModule.forRoot(ormConfig),
     FilesModule,
     CountriesModule,
+    MetasModule,
   ],
 })
 export class AppModule {}
