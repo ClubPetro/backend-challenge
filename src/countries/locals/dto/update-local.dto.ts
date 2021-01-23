c614ff249498bebed866ee1f69a7eaa73a23d503
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-23 17:44:31 -0300

    Criar update-local dto

--- src/countries/locals/dto/update-local.dto.ts
+++ src/countries/locals/dto/update-local.dto.ts
@@ -1,11 +1,13 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString, IsOptional } from 'class-validator';
 
 import Country from '../../country.entity';
 
 export class UpdateLocalDto {
   @IsString()
+  @IsOptional()
   name?: string;
 
   @IsNumber()
+  @IsOptional()
   countryId?: Country;
 }
