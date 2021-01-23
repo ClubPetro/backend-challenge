8131aee9dc86c077cb1dfeda2ed0cf4401c8794e
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-23 17:44:04 -0300

    Contornar dependência circular com forwardRef

--- src/countries/countries.module.ts
+++ src/countries/countries.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Country from './country.entity';
 import { CountriesController } from './countries.controller';
 import { CountriesService } from './countries.service';
 import { FilesModule } from '../files/files.module';
 import { LocalsModule } from './locals/locals.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Country]), FilesModule, LocalsModule],
+  imports: [
+    TypeOrmModule.forFeature([Country]),
+    FilesModule,
+    forwardRef(() => LocalsModule),
+  ],
   controllers: [CountriesController],
   providers: [CountriesService],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, CountriesService],
 })
 export class CountriesModule {}
--- src/countries/locals/locals.module.ts
+++ src/countries/locals/locals.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocalsService } from './locals.service';
 import { LocalsController } from './locals.controller';
+import { CountriesModule } from '../countries.module';
 import Local from './local.entity';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Local])],
-  providers: [LocalsService],
+  imports: [
+    TypeOrmModule.forFeature([Local]),
+    forwardRef(() => CountriesModule),
+  ],
   controllers: [LocalsController],
+  providers: [LocalsService],
+  exports: [TypeOrmModule, LocalsService],
 })
 export class LocalsModule {}
