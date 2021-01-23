1e37335185e9892845cf557338af8e9530e62853
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-23 17:44:22 -0300

    Criar novas funcionalidades em countries service

--- src/countries/countries.service.ts
+++ src/countries/countries.service.ts
@@ -1,5 +1,7 @@
 import {
   BadRequestException,
+  forwardRef,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -7,28 +9,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCountryDto } from './dto/create-country.dto';
 import Country from './country.entity';
-import File from '../files/file.entity';
+import { FilesService } from 'src/files/files.service';
+import { LocalsService } from './locals/locals.service';
+import Local from './locals/local.entity';
 
 @Injectable()
 export class CountriesService {
   constructor(
     @InjectRepository(Country)
     private countryRepository: Repository<Country>,
-    @InjectRepository(File)
-    private fileRepository: Repository<File>,
+    @Inject(FilesService)
+    private fileService: FilesService,
+    @Inject(forwardRef(() => LocalsService))
+    private localService: LocalsService,
   ) {}
 
   public async create(createCountryDto: CreateCountryDto): Promise<Country> {
     const { name, fileId } = createCountryDto;
+
+    /*Validacao para ver se o id de arquivo enviado existe, retornando o arquivo*/
+    const file = await this.fileService.findOne(String(fileId));
+
     const country = new Country();
     country.name = name;
-    country.file = fileId;
-
-    /*Validacao para ver se o id de arquivo enviado existe*/
-    const existsFile = await this.fileRepository.findOne(fileId);
-    if (!existsFile) {
-      throw new NotFoundException(`file with id ${fileId} not found!`);
-    }
+    country.file = file;
 
     /*Validacao para garantir que + de um pais nao esteja associado a um mesmo arquivo (bandeira)*/
     const existsFileAssociatedCountry = await this.countryRepository.findOne({
@@ -59,7 +63,7 @@ export class CountriesService {
   public async findOne(id: string): Promise<Country> {
     const country = await this.countryRepository.findOne(id);
     if (!country) {
-      throw new NotFoundException(`Country whith id ${id} not found`);
+      throw new NotFoundException(`Country with id ${id} not found`);
     }
     return country;
   }
@@ -67,11 +71,27 @@ export class CountriesService {
   public async remove(id: string): Promise<void> {
     const country = await this.countryRepository.findOne(id);
     if (!country) {
-      throw new NotFoundException(`Country whith id ${id} not found`);
+      throw new NotFoundException(`Country with id ${id} not found`);
     }
-    if (country.file) {
-      await this.fileRepository.remove(country.file);
-    }
-    await this.countryRepository.delete(id);
+    await this.fileService.remove(String(country.file.id));
+    await this.countryRepository.delete(country);
+  }
+
+  public async verifyLocalNameExistsInCountry(
+    localName: string,
+    country: Country,
+  ): Promise<boolean> {
+    const countryLocals = await this.findAllLocalsInCountry(String(country.id));
+    const exists = countryLocals.find((local) => local.name === localName);
+    return !!exists;
+  }
+
+  public async findAllLocalsInCountry(countryId: string): Promise<Local[]> {
+    await this.findOne(countryId);
+    const locals = await this.localService.findAll();
+    const countryLocals = locals.filter(
+      (local) => local.country.id === Number(countryId),
+    );
+    return countryLocals;
   }
 }
