98a93cf430b8a947a2cfe0bed2df5bfb73fc86b0
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-24 10:15:48 -0300

    Remover MulterModule

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MulterModule } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 import File from './file.entity';
-import * as uploadConfig from '../config/upload.config';
 
 @Module({
-  imports: [
-    MulterModule.register({
-      storage: uploadConfig.default.storage,
-      dest: uploadConfig.default.directory,
-      fileFilter: uploadConfig.default.fileFilter,
-    }),
-    TypeOrmModule.forFeature([File]),
-  ],
+  imports: [TypeOrmModule.forFeature([File])],
   providers: [FilesService],
   controllers: [FilesController],
   exports: [TypeOrmModule, FilesService],
