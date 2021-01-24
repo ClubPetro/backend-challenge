b66e8b8085f828b1f8a5bb254aa82864fdbe51ec
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-23 23:02:44 -0300

    Adicionar módulos em meta

--- src/metas/metas.module.ts
+++ src/metas/metas.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MetasController } from './metas.controller';
 import { MetasService } from './metas.service';
+import { LocalsModule } from '../countries/locals/locals.module';
+import Meta from './meta.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Meta]), LocalsModule],
   controllers: [MetasController],
-  providers: [MetasService]
+  providers: [MetasService],
+  exports: [TypeOrmModule],
 })
 export class MetasModule {}
