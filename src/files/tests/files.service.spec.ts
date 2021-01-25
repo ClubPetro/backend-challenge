d323093b6d083eed6700a3ee0d9eab60ac500001
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-24 21:15:55 -0300

    Desenvolver testes para FileService

--- src/files/tests/files.service.spec.ts
+++ src/files/tests/files.service.spec.ts
@@ -1,18 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { FilesService } from '../files.service';
+import File from '../file.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('FilesService', () => {
-  let service: FilesService;
-
+  let filesService: FilesService;
+  let findOne: jest.Mock;
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [FilesService],
+    const module = await Test.createTestingModule({
+      providers: [
+        FilesService,
+        {
+          provide: getRepositoryToken(File),
+          useValue: { findOne },
+        },
+      ],
     }).compile();
-
-    service = module.get<FilesService>(FilesService);
+    filesService = await module.get(FilesService);
   });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('when getting a user by email', () => {
+    describe('and the user is matched', () => {
+      let file: File;
+      beforeEach(() => {
+        const hashedName = 'sdcksdkclsdc12354scdsdc7.jpg';
+        file = new File(hashedName);
+        findOne.mockReturnValue(Promise.resolve(file));
+      });
+      it('should return the file', async () => {
+        const fetchedFile = await filesService.findOne('1');
+        expect(fetchedFile).toEqual(file);
+      });
+    });
+    describe('and the user is not matched', () => {
+      beforeEach(() => {
+        findOne.mockReturnValue(undefined);
+      });
+      it('should throw an error', async () => {
+        await expect(filesService.findOne('150')).rejects.toThrow();
+      });
+    });
   });
 });
