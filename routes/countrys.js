532ec09515c57c5d102c9fbabb7f2be1ad4a532e
Wandinho Oliveira <inovaprog@gmail.com>
2021-01-30 17:16:50 -0300

    Rotas de Paises

--- routes/countrys.js
+++ routes/countrys.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db')
 
-router.get('/', function(req, res) {
-  db.findAllCountrys((err, countrys) =>{
-    if (err){
+router.get('/', function (req, res) {
+  db.findAllCountrys((err, countrys) => {
+    if (err) {
       return console.log(err)
     }
     res.json(countrys)
   })
 });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   var id = req.params.id
-  db.findOneCountry(id, (err, countrys) =>{
-    if (err){
+  db.findOneCountry(id, (err, countrys) => {
+    if (err) {
       return console.log(err)
     }
     res.json(countrys)
@@ -25,14 +25,33 @@ router.post('/', function (req, res) {
 
   var nome = req.body.nome
   var url_bandeira = req.body.url_bandeira
-  
-  db.addCountry({ nome, url_bandeira }, (err, result) => {
-      if (err) { 
-        return console.log(err)
-       }
-        res.json(result.ops)
-  })
 
-  
+  db.findCountryNome(nome, (err, country) => {
+    if (err) {
+      return console.log(err)
+    }
+    if (!country[0]) {
+      db.addCountry({ nome, url_bandeira }, (err, result) => {
+        if (err) {
+          return console.log(err)
+        }
+        res.json(result.ops)
+      })
+    }
+    else {
+      res.status(400).send("Ja Cadastrado")
+    }
+
+  })
 })
+router.delete('/:id', function (req, res) {
+  var id = req.params.id
+  db.delCountry(id, (err, result) => {
+    if (err) {
+      return console.log(err)
+    }
+    res.json(result)
+  })
+});
+
 module.exports = router;
