e49bad516c5fa16459a8d5ba885c3e9f344268b1
Wandinho Oliveira <inovaprog@gmail.com>
2021-01-30 17:16:25 -0300

    Variaveis de Ambiente

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+var env = require('dotenv').config()
 var mongoClient = require("mongodb").MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 
 
-mongoClient.connect("mongodb+srv://clubpetro:clubpetro@cluster0.d4gur.mongodb.net/desafio?retryWrites=true&w=majority", { useUnifiedTopology: true })
-    .then(conn => global.conn = conn.db("desafio"))
+mongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+    .then(conn => global.conn = conn.db(process.env.MONGODB_DB))
     .catch(err => console.log(err))
 
 function add(item, callback) {
@@ -22,20 +23,22 @@ function findOne(id, callback) {
     global.conn.collection("places").find({ _id: new ObjectId(id) }).toArray(callback);
 }
 
-function findPlaceCountry(country_id, callback) {
-    global.conn.collection("places").find({ country_id: country_id }).toArray(callback);
+function findPlaceCountry(consulta, callback) {
+    global.conn.collection("places").find({ country_id: consulta.country_id, place: consulta.place }).toArray(callback);
 }
 
 function edit(item, callback) {
 
     id = item.id
-    local = item.local
+    place = item.place
     meta = item.meta
+    data_edicao = item.data_edicao
 
     global.conn.collection("places").updateOne({ _id: new ObjectId(id) }, {
         $set: {
-            local: local,
+            place: place,
             meta: meta,
+            data_edicao: data_edicao,
         }
     }, callback);
 
@@ -46,11 +49,18 @@ function addCountry(item, callback) {
 }
 
 function findAllCountrys(callback) {
-    global.conn.collection("countrys").find({}).toArray(callback);
+    global.conn.collection("countrys").find({}).sort({ nome: 1 }).toArray(callback);
+}
+
+function findCountryNome(nome, callback) {
+    global.conn.collection("countrys").find({ nome: nome }).sort({ nome: 1 }).toArray(callback);
 }
 
 function findOneCountry(id, callback) {
-    console.log(id)
     global.conn.collection("countrys").find({ _id: new ObjectId(id) }).toArray(callback);
 }
-module.exports = { add, del, findOne, edit, findAll, addCountry, findAllCountrys, findOneCountry, findPlaceCountry };
+
+function delCountry(id, callback) {
+    global.conn.collection("countrys").deleteOne({ _id: new ObjectId(id) }, callback);
+}
+module.exports = { add, del, findOne, edit, findAll, addCountry, findAllCountrys, findOneCountry, findPlaceCountry, findCountryNome, delCountry };
