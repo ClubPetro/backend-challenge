9c89b776a3a6541c55ace7290c86f3b09a3239a3
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-24 13:58:09 -0300

    Places table columns

--- src/database/migrations/1621875195602-CreatePlaces.ts
+++ src/database/migrations/1621875195602-CreatePlaces.ts
@@ -1,11 +1,48 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreatePlaces1621875195602 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: "Places",
+        "columns": [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "country",
+            type: "varchar",
+          },
+          {
+            name: "url_flag",
+            type: "varchar",
+          },
+          {
+            name: "goal",
+            type: "timestamp",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ]
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("Places");
     }
 
 }
