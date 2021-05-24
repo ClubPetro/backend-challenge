27c497fa7c6cfc158b1cb860dfac1fa9ac30d37d
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-24 16:25:31 -0300

    List places by goal use case

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreatePlaceController } from "./modules/places/useCases/createPlaceUseCase/CreatePlaceController";
+import { ListPlacesController } from "./modules/places/useCases/listPlacesUseCase/ListPlacesController";
 
 const routes = Router();
 
 const createPlaceController = new CreatePlaceController();
+const listPlaceController = new ListPlacesController();
 
 routes.post("/places", createPlaceController.handle);
+routes.get("/places", listPlaceController.handle);
 
 export { routes };
