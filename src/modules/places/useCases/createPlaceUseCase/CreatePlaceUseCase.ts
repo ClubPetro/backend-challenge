07cd2a68d553e11a780b0e47e50329b8bb484e2c
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-25 11:08:52 -0300

    crate variable to work with country repository

--- src/modules/places/useCases/createPlaceUseCase/CreatePlaceUseCase.ts
+++ src/modules/places/useCases/createPlaceUseCase/CreatePlaceUseCase.ts
@@ -3,15 +3,19 @@ import { inject, injectable } from "tsyringe";
 import { AppError } from "../../../../errors/AppError";
 import { ICreatePlaceDTO } from "../../dtos/ICreatePlaceDTO";
 import { Place } from "../../entities/Places";
+import { ICountryCodeRepository } from "../../repositories/ICountryCodeRepository";
 import { CountryCodeRepository } from "../../repositories/implementations/CountryCodeRepository";
 import { IPlacesRepository } from "../../repositories/IPlacesRepository";
 
 @injectable()
 class CreatePlaceUseCase {
+  private countryRepository: ICountryCodeRepository;
   constructor(
     @inject("PlacesRepository")
     private placesRepository: IPlacesRepository
-  ) {}
+  ) {
+    this.countryRepository = new CountryCodeRepository();
+  }
 
   async execute({ name, country, goal }: ICreatePlaceDTO): Promise<Place> {
     const isNameInUseInCountry = await this.placesRepository.findByCountry(
@@ -23,8 +27,11 @@ class CreatePlaceUseCase {
       throw new AppError("Place name already in use in that country.");
     }
 
-    const codesRepository = new CountryCodeRepository();
-    const country_code = await codesRepository.findCodeByName(country);
+    const country_code = await this.countryRepository.findCodeByName(country);
+
+    if (!country_code) {
+      throw new AppError("Not able to get country code");
+    }
 
     const url_flag = `https://flagcdn.com/h240/${country_code.toLowerCase()}.png`;
 
