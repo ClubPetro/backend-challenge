4189c88a37cf0ca77287ea581d623c56cade1ac5
mathews <mathews@sway.com.br>
2022-03-11 11:16:16 -0300

    using .env for port also

--- places-api/src/main.ts
+++ places-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.APP_PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
