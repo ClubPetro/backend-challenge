19ba25d300fee3fe872f456136fc4c7fedcf10c5
mathews <mathews@sway.com.br>
2022-03-11 11:31:11 -0300

    config module set as global

--- places-api/src/app.module.ts
+++ places-api/src/app.module.ts
@@ -4,7 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
