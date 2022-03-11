bccc43c78b1a0fad705840d7915a14a4182a5036
mathews <mathews@sway.com.br>
2022-03-11 11:37:19 -0300

    generating swagger doc

--- places-api/src/main.ts
+++ places-api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const PORT = process.env.APP_PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Places API')
+    .setDescription('Places CRUD Api')
+    .setVersion('1.0')
+    .addTag('places')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
