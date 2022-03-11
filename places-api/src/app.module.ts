8a9aa81752d5c4190716ab6a304fedebe65e818c
mathews <mathews@sway.com.br>
2022-03-11 09:38:40 -0300

    configModule imported into app.module.ts

--- places-api/src/app.module.ts
+++ places-api/src/app.module.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,4 +8,4 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
