00f94480bdd0ac21363604c060ecace5d409414b
mathews <mathews@sway.com.br>
2022-03-11 09:39:41 -0300

    using .env now

--- places-api/src/app.module.ts
+++ places-api/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
