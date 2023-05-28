e3b5ec3afc5ef0dd14e4d65bb6fe3a6c0185313e
Patrick Ferreira <patrickcorrea.f@gmail.com>
2023-05-28 14:07:06 -0300

    feature: add modules in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import DatabaseModule from './infra/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { CountryModule } from './modules/country/country.module';
+import { ObjectiveModule } from './modules/objective/objective.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    CountryModule,
+    ObjectiveModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
