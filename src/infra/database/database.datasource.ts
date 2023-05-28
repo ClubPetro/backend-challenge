745126ba6e71b4071cdf119c4b8ab3cc8078bbb8
Patrick Ferreira <patrickcorrea.f@gmail.com>
2023-05-28 13:57:34 -0300

    chore: create data source for typeorm

---
+++ src/infra/database/database.datasource.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get('POSTGRES_HOST'),
+  port: configService.get('POSTGRES_PORT'),
+  username: configService.get('POSTGRES_USER'),
+  password: configService.get('POSTGRES_PASSWORD'),
+  database: configService.get('POSTGRES_DATABASE'),
+  synchronize: false,
+  migrations: [`${__dirname}/migrations/*.{ts|js}`],
+});
