da383d9379d8d8e862c01dc46d7be853c632c911
Patrick Ferreira <patrickcorrea.f@gmail.com>
2023-05-28 14:07:26 -0300

    add global prefix and port configuration

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT);
 }
 bootstrap();
